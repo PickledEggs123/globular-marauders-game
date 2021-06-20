a74d98331bf0678ec5d3ddf7cb69954b84f490dd
Tyler Truong <tyler_truong@icloud.com>
2021-06-19 20:20:53 -0400

    Added re-exported definitions.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export {Game} from "./Game";
+export {Faction} from "./Faction";
+export {Planet, Star, Building} from "./Planet";
+export {Order} from "./Order";
+export {Ship} from "./Ship";
+export {VoronoiTerrain, VoronoiKingdom, VoronoiDuchy, VoronoiCounty} from "./VoronoiTree";
