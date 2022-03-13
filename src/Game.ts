93e30b5576e4d72531dc07811897e0efa32bf648
Tyler Truong <tyler_truong@icloud.com>
2022-03-12 22:36:53 -0500

    Adjusted physics to match render for Quaternion cannonball bug. Attempt 3

--- src/Game.ts
+++ src/Game.ts
@@ -930,8 +930,8 @@ export class Game {
         }
         if (activeKeys.includes("w")) {
             const positionPoint = cameraPosition.clone().rotateVector([0, 0, 1]);
-            const positionTheta = Math.atan2(positionPoint[1], positionPoint[0]);
-            const forward = Quaternion.fromAxisAngle([0, 0, 1], positionTheta).mul(cameraOrientation.clone()).rotateVector([0, 1, 0]);
+            const positionTheta = Math.atan2(positionPoint[0], positionPoint[1]);
+            const forward = Quaternion.fromAxisAngle([0, 0, 1], -positionTheta).mul(cameraOrientation.clone()).rotateVector([0, 1, 0]);
             const rotation = Quaternion.fromBetweenVectors([0, 0, 1], forward).pow(velocityAcceleration / this.worldScale);
             const rotationDrag = cameraPositionVelocity.pow(velocitySpeed / this.worldScale).inverse();
             cameraPositionVelocity = cameraPositionVelocity.clone().mul(rotation).mul(rotationDrag);
@@ -964,8 +964,8 @@ export class Game {
                 jitterPoint[1] += DelaunayGraph.randomInt() * 0.15;
                 jitterPoint = DelaunayGraph.normalize(jitterPoint);
                 const positionPoint = cameraPosition.clone().rotateVector([0, 0, 1]);
-                const positionTheta = Math.atan2(positionPoint[1], positionPoint[0]);
-                const fireDirection = Quaternion.fromAxisAngle([0, 0, 1], positionTheta).mul(cameraOrientation.clone()).rotateVector(jitterPoint);
+                const positionTheta = Math.atan2(positionPoint[0], positionPoint[1]);
+                const fireDirection = Quaternion.fromAxisAngle([0, 0, 1], -positionTheta).mul(cameraOrientation.clone()).rotateVector(jitterPoint);
                 const fireVelocity = Quaternion.fromBetweenVectors([0, 0, 1], fireDirection).pow(Game.PROJECTILE_SPEED / this.worldScale);
 
                 // create a cannon ball
